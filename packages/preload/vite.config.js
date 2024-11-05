9d188558e6c96a52237868167ee7f8fae236d2ab
sway913 <644914747@qq.com>
2024-11-05 17:35:41 +0800

    Initial commit

---
+++ packages/preload/vite.config.js
@@ -0,0 +1,75 @@
+import {resolveModuleExportNames} from 'mlly';
+
+export default /**
+ * @type {import('vite').UserConfig}
+ * @see https://vitejs.dev/config/
+ */
+({
+  build: {
+    ssr: true,
+    sourcemap: 'inline',
+    outDir: 'dist',
+    assetsDir: '.',
+    lib: {
+      entry: ['src/exposed.ts', 'virtual:browser.js'],
+    },
+    rollupOptions: {
+      output: [
+        {
+          // ESM preload scripts must have the .mjs extension
+          // https://www.electronjs.org/docs/latest/tutorial/esm#esm-preload-scripts-must-have-the-mjs-extension
+          entryFileNames: '[name].mjs',
+        },
+      ],
+    },
+    emptyOutDir: true,
+    reportCompressedSize: false,
+  },
+  plugins: [mockExposed()],
+});
+
+/**
+ * This plugin creates a browser (renderer) version of `preload` package.
+ * Basically, it just read all nominals you exported from package and define it as globalThis properties
+ * expecting that real values were exposed by `electron.contextBridge.exposeInMainWorld()`
+ *
+ * Example:
+ * ```ts
+ * // index.ts
+ * export const someVar = 'my-value';
+ * ```
+ *
+ * Output
+ * ```js
+ * // _virtual_browser.mjs
+ * export const someVar = globalThis[<hash>] // 'my-value'
+ * ```
+ */
+function mockExposed() {
+  const virtualModuleId = 'virtual:browser.js';
+  const resolvedVirtualModuleId = '\0' + virtualModuleId;
+
+  return {
+    name: 'electron-main-exposer',
+    resolveId(id) {
+      if (id.endsWith(virtualModuleId)) {
+        return resolvedVirtualModuleId;
+      }
+    },
+    async load(id) {
+      if (id === resolvedVirtualModuleId) {
+        const exportedNames = await resolveModuleExportNames('./src/index.ts', {
+          url: import.meta.url,
+        });
+        return exportedNames.reduce((s, key) => {
+          return (
+            s +
+            (key === 'default'
+              ? `export default globalThis['${btoa(key)}'];\n`
+              : `export const ${key} = globalThis['${btoa(key)}'];\n`)
+          );
+        }, '');
+      }
+    },
+  };
+}
